6509f173fa1f15d0b62c4e71161b2c8c9cf442f5
Beloved <1425279634@qq.com>
2022-03-14 23:04:30 +0800

    自定义登录页

--- SpringSecurity/spring-security-01/src/main/java/com/beloved/config/WebSecurityConfigurer.java
+++ SpringSecurity/spring-security-01/src/main/java/com/beloved/config/WebSecurityConfigurer.java
@@ -10,9 +10,18 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/index").permitAll()  // 放行资源要写在认证之前
+                .mvcMatchers("/index","/login.html").permitAll()  // 放行资源要写在认证之前
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login.html")   // 指定登录页面
+                .loginProcessingUrl("/doLogin") // 指定了自定义登录页面必须指定登录请求地址
+                .usernameParameter("uname").passwordParameter("pwd") // 指定表单字段名
+                //.successForwardUrl("/index") // 认证成功跳转地址    forward
+                //.defaultSuccessUrl("/index")   // 认证成功跳转地址    redirect 重定向  注意：请求之前有地址，会有优先跳转之前的地址
+                .defaultSuccessUrl("/index", true)  // 无论请求之前是否有地址都进行重定向
+                .and()
+                .csrf().disable()   // 禁止 csrf 跨站请求攻击保护
+        ;
     }
 }
