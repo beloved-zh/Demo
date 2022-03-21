f3f86f08d50adb1d0b0d435752b3578320217628
张恒 <1425279634@qq.com>
2022-03-21 21:18:52 +0800

    认证用户信息获取

--- SpringSecurity/spring-security-01/src/main/java/com/beloved/controller/HelloController.java
+++ SpringSecurity/spring-security-01/src/main/java/com/beloved/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.beloved.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,19 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
+        // 获取认证信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User)authentication.getPrincipal();
+        System.out.println("身份信息：" + user.getUsername());
+        System.out.println("权限信息：" + authentication.getAuthorities());
+
+        new Thread(() -> {
+            Authentication authentication1 = SecurityContextHolder.getContext().getAuthentication();
+            User user1 = (User)authentication1.getPrincipal();
+            System.out.println("t1身份信息：" + user1.getUsername());
+            System.out.println("t1权限信息：" + authentication1.getAuthorities());
+        }, "t1").start();
+
         return "Hello SpringSecurity！";
     }
 
