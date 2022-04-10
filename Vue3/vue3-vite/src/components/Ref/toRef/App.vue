0d37ef1104d9633984544f762bfda49c305d3e7b
张恒 <1425279634@qq.com>
2022-04-10 21:22:31 +0800

    toRef toRefs toRaw

---
+++ Vue3/vue3-vite/src/components/Ref/toRef/App.vue
@@ -0,0 +1,29 @@
+<template>
+  Object：{{user}}<br><br>
+  wife：{{wife}}<br><br>
+  <button @click="update">更新数据</button>
+</template>
+
+<script setup lang="ts">
+  import { reactive, toRef } from 'vue'
+
+  let user = reactive({
+    name: '张三',
+    wife: {
+      name: '小红'
+    }
+  })
+
+  // 从源响应式对象上的某个 property 结构一个 ref
+  let wife = toRef(user, 'wife')
+
+  const update = () => {
+
+    // 修改结构对象也会改变原始对象
+    wife.value.name += '!'
+
+    console.log('wife', wife);
+    console.log('user：', user);  
+  }
+
+</script>
---
+++ Vue3/vue3-vite/src/components/Ref/toRefs/App.vue
@@ -0,0 +1,32 @@
+<template>
+  Object：{{user}}<br><br>
+  name：{{name}}<br><br>
+  name：{{wife}}<br><br>
+  <button @click="update">更新数据</button>
+</template>
+
+<script setup lang="ts">
+  import { reactive, toRefs } from 'vue'
+
+  let user = reactive({
+    name: '张三',
+    wife: {
+      name: '小红'
+    }
+  })
+
+  // 将源响应式对象上的所有 property 结构成 ref
+  let {name, wife} = toRefs(user)
+
+  const update = () => {
+
+    // 修改结构对象也会改变原始对象
+    name.value += '1'
+    wife.value.name += '!'
+
+    console.log('name', name);
+    console.log('wife', wife);
+    console.log('user：', user);  
+  }
+
+</script>
