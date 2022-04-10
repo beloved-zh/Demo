22385377ebd1ba6e257a6c40cea2f16e4ba49432
张恒 <1425279634@qq.com>
2022-04-10 19:23:17 +0800

    reactive相关api

---
+++ Vue3/vue3-vite/src/components/Reactive/reactive/App.vue
@@ -0,0 +1,38 @@
+<template>
+  Object：{{user}}<br><br>
+  Array：{{nameList}}<br><br>
+  <button @click="update">更新数据</button>
+</template>
+
+<script setup lang="ts">
+  import { reactive } from 'vue'
+
+  // reactive 不能定义基本数据类似
+  // let number = reactive(10)
+
+  let user = reactive({
+    name: 'zhngsan',
+    age: 20
+  })
+
+  let nameList = reactive(['张三', '李四'])
+
+  const update = () => {
+    // 直接赋值会破坏响应式
+    // user = {
+    //   name: '李四',
+    //   age: 22
+    // }
+
+    // nameList = ['王五']
+
+    // reactive 属性操作不需要 .value
+    user.name = '李四'
+    user.age = 22
+    nameList.push('王五')
+
+    console.log('user：', user)
+    console.log('nameList', nameList)
+  }
+
+</script>
---
+++ Vue3/vue3-vite/src/components/Reactive/readonly/App.vue
@@ -0,0 +1,24 @@
+<template>
+  Object：{{user}}<br><br>
+  <button @click="update">更新数据</button>
+</template>
+
+<script setup lang="ts">
+  import { reactive, readonly } from 'vue'
+
+  let user = reactive({
+    name: 'zhngsan',
+    age: 20
+  })
+
+  // 拷贝一份proxy对象将其设置为只读
+  let copyUser = readonly(user)
+
+  const update = () => {
+    user.age = 22
+
+    // 只读属性不能被修改
+    // copyUser.age =30
+  }
+
+</script>
