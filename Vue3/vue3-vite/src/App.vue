188c6e25ea02a60de01d52340fc1e5d1e2b2d69f
Beloved <1425279634@qq.com>
2022-04-12 17:57:14 +0800

    <script setup> emit

--- Vue3/vue3-vite/src/App.vue
+++ Vue3/vue3-vite/src/App.vue
@@ -1,7 +1,18 @@
 <template>
-
+  <h1>父组件</h1>
+  number：{{number}}<br><br>
+  <Child @on-click="getNumber"></Child>
 </template>
 
 <script setup lang="ts">
+  import Child from './Child.vue'
+
+  import { ref } from 'vue'
+
+  let number = ref()
+
+  const getNumber = (val: number) => {
+    number.value = val
+  }
 
 </script>
---
+++ Vue3/vue3-vite/src/Child.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>子组件</h2>
+  number：{{number}} <br><br>
+  <button @click="addNumber">点击增加传递给父组件</button>
+</template>
+
+<script setup lang="ts">
+
+  import {ref} from 'vue'
+
+  let number = ref(10)
+
+  // defineEmits 定义自定义事件
+  const emit = defineEmits(['on-click'])
+
+  const addNumber = () => {
+    number.value++
+    // 调用
+    emit('on-click', number)
+  }
+</script>
