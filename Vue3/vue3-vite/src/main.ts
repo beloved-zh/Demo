a59b997fd51afc5b5d2ce27fc4d29214d2be5d25
Beloved <1425279634@qq.com>
2022-04-21 16:44:09 +0800

    globalProperties

--- Vue3/vue3-vite/src/main.ts
+++ Vue3/vue3-vite/src/main.ts
@@ -7,14 +7,29 @@ const Mitt = mitt()
 
 const app = createApp(App)
 
+
+type Filter = {
+    format: <T extends any>(str: T) => T
+}
+
 // 扩展 globalProperties 声明 
 declare module 'vue' {
     export interface ComponentCustomProperties {
-        $EventBus: typeof Mitt
+        $EventBus: typeof Mitt,
+        $filters: Filter,
+        $userName: string
     }
 }
 
 // 挂载全局实例，要挂载在config.globalProperties上
 app.config.globalProperties.$EventBus = Mitt
+
+// 定义全局方法、变量
+app.config.globalProperties.$filters = {
+    format<T extends any>(str: T): string {
+        return `${str}---${str}`
+    }
+}
+app.config.globalProperties.$userName = 'Beloved'
 
 app.mount('#app')
