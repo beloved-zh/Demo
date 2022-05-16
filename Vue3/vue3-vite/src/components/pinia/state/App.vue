31d37ab5f5b83ab139f75199642650000a71e2e2
Beloved <1425279634@qq.com>
2022-05-16 13:49:23 +0800

    pinia getters

--- Vue3/vue3-vite/src/components/pinia/state/App.vue
+++ Vue3/vue3-vite/src/components/pinia/state/App.vue
@@ -1,10 +1,10 @@
 <template>
   <h1>App.vue</h1>
-  count：{{store.count}} <button @click="countAdd">count++</button><br><br>
+  age：{{store.age}} <button @click="ageAdd">age++</button><br><br>
   username：{{store.username}} <br><br>
   <hr>
   <h3>storeToRefs解构</h3>
-  count：{{count}} <button @click="countAdd2">count++</button><br><br>
+  age：{{age}} <button @click="ageAdd2">age++</button><br><br>
   username：{{username}} <br><br><hr>
 </template>
 
@@ -16,42 +16,42 @@
   const store = useTestStore()
   
   // pinia 直接解构失去响应式
-  // const { count, username } = store
+  // const { age, username } = store
   
-  const { count, username } = storeToRefs(store)
+  const { age, username } = storeToRefs(store)
   
-  const countAdd = () => {
+  const ageAdd = () => {
     // 1：直接修改
-    //  store.count++
+    //  store.age++
     
     // 2：通过 $patch 修改，传递一个对象，可以有多个，对象中没有的则不会修改
     // store.$patch({
-    //   count: 888,
+    //   age: 888,
     //   username: '李四'
     // })
     
     // 3：通过 $patch 修改，传递一个函数，可以在修改时做逻辑处理，参数是 store 的 state
     // store.$patch((state) => {
     //   console.log('state', state)
-    //   if (state.count > 0) {
-    //     state.count--
+    //   if (state.age > 0) {
+    //     state.age--
     //   } else {
-    //     state.count++
+    //     state.age++
     //   }
     // })
 
     // 4：通过 $state 修改，必须覆盖整个 state 对象
     // store.$state = {
-    //   count: 10,
+    //   age: 10,
     //   username: '李四'
     // }
     
     // 5：通过 action 
-    store.addCount(10)
+    store.addAge(10)
   }
   
-  const countAdd2 = () => {
-    count.value++
+  const ageAdd2 = () => {
+    age.value++
   }
 </script>
 
--- Vue3/vue3-vite/src/store/index.ts
+++ Vue3/vue3-vite/src/store/index.ts
@@ -6,18 +6,29 @@ import { defineStore } from 'pinia'
 export const useTestStore = defineStore('TEST', {
     state: () => {
         return {
-            count: 1,
+            age: 1,
             username: '张三'
         }
     },
     // 等效于 计算属性
     getters: {
-        
+        // 使用箭头函数后 不能使用 this，需要使用 state
+        nameAge: (state) => {
+            return `${state.username}---${state.age}`
+        },
+        // 普通函数 可以使用 this
+        sex(): string {
+          return this.age % 2 === 0 ? '男' : '女'  
+        },
+        // 互相调用
+        nameAgeSex() {
+          return `${this.nameAge}---${this.sex}`  
+        }
     },
     // 等效于 方法，可以做同步异步
     actions: {
-        addCount (num:number) {
-            this.count += num
+        addAge (num:number) {
+            this.age += num
         }
     }
 })
